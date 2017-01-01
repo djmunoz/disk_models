70235bbba2c3278cd878f2e371c8319ddfbe64fc
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-01-01 16:52:25 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+from distutils.core import setup
+import numpy as np
+import os
+import sys
+
+
+setup(
+    name='disk_3d_models',
+    version="0.0.1",
+    author='Diego J. Munoz',
+    author_email = 'diego.munoz.anguita@gmail.com',
+    url='https://github.com/',
+    packages=['disk_3d_models','hdf5'],
+    description='Setup three-dimensional models of accretion disks in hydrostatic and centrifugal equilibrium'
+    install_requires = ['numpy','scipy','tables'],
+)
