bd7da3f8b0e05ac45e7076a9a8140f8bbeef4a86
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-07-07 16:37:43 -0700

    Added Spline softening of the Keplerian potential

--- disk_3d_models/disk_structure.py
+++ disk_3d_models/disk_structure.py
@@ -18,33 +18,31 @@ rd.seed(42)
 
 def SplineProfile(R,h):
   r2 = R*R
-  if(R >= h):
-    wp = - 1.0 / R
-  else:
-    h_inv = 1.0 / h
-    h3_inv = h_inv * h_inv * h_inv
-    u = R * h_inv
-    if(u < 0.5):
-      wp =  h_inv * (-2.8 + u * u * (5.333333333333 + u * u * (6.4 * u - 9.6)))
-    else:
-      wp = h_inv * (-3.2 + 0.066666666667 / u + u * u * (10.666666666667 + u * (-16.0 + u * (9.6 - 2.133333333333 * u))))
+  h_inv = 1.0 / h
+  h3_inv = h_inv * h_inv * h_inv
+  u = R * h_inv
+  wp = -1.0 / R
+
+  ind = u < 0.5
+  wp[ind] = h_inv * (-2.8 + u[ind] * u[ind] * (5.333333333333 + u[ind] * u[ind] * (6.4 * u[ind] - 9.6)))
+  ind = (u >= 0.5) & (u < 1)
+  wp[ind] = h_inv * (-3.2 + 0.066666666667 / u[ind] + u[ind] * u[ind] * (10.666666666667 + u[ind] * (-16.0 + u[ind] * (9.6 - 2.133333333333 * u[ind]))))
       
   return -wp
   
 def SplineDerivative(R,h):
   r2 = R * R
   fac = 0.0
-  if(R >= h):
-    fac = 1.0 / (r2 * R)
-  else:
-    h_inv = 1.0 / h
-    h3_inv = h_inv * h_inv * h_inv
-    u = R * h_inv
-    if(u < 0.5):
-      fac = h3_inv * (10.666666666667 + u * u * (32.0 * u - 38.4))
-    else:
-      fac = h3_inv * (21.333333333333 - 48.0 * u + 38.4 * u * u - 10.666666666667 * u * u * u - 0.066666666667 / \
-                      (u * u * u))
+  h_inv = 1.0 / h
+  h3_inv = h_inv * h_inv * h_inv
+  u = R * h_inv
+  
+  fac = 1.0 / (r2 * R)
+  ind = u < 0.5
+  fac[ind] = h3_inv * (10.666666666667 + u[ind] * u[ind] * (32.0 * u[ind] - 38.4))
+  ind = (u >= 0.5) & (u < 1)
+  fac[ind] = h3_inv * (21.333333333333 - 48.0 * u[ind] + 38.4 * u[ind] * u[ind] - 10.666666666667 * u[ind] * u[ind] * u[ind] - 0.066666666667 / \
+                  (u[ind] * u[ind] * u[ind]))
       
   return fac
 
